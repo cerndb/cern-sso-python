9b5f8fc4d007a85bfed988395844556126d3e3de
Albin Stjerna <albin.stjerna@cern.ch>
2017-01-31 14:09:05 +0100

    Add proper unit tests

--- test_cern_sso.py
+++ test_cern_sso.py
@@ -1,4 +1,15 @@
 import cern_sso
+import requests
+
+TEST_URL = "https://dbnas-storage-docs.web.cern.ch"
+
 
 def test_do_nothing():
     pass
+
+
+def test_krb_signin():
+    cookies = cern_sso.krb_sign_on(TEST_URL)
+
+    r1 = requests.get(TEST_URL, cookies=cookies)
+    assert r1.status_code == 200
